8ca92b7fd5223dc3688c1eb741ad291d571b8c1f
Noah Zoschke <noah@mixable.net>
2019-10-11 08:21:32 -0700

    yamllint

---
+++ yamllint/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh -l
+set -ex
+
+CONFIG=${1:-"{extends: default}"}
+STRICT=${2:-true}
+FILE=${3:-.}
+
+[ "$STRICT" = "true" ] && STRICT="-s" || STRICT=""
+
+yamllint -d "$CONFIG" $STRICT $FILE
